eb47ef1d53462872f5b0616772443d14e2f5e2f9
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2026-06-10 08:21:02 +0530

    feat: add method  transform_data in iris_flower_classification.py

--- src/services/iris_flower_data_loader.py
+++ src/services/iris_flower_data_loader.py
@@ -1,4 +1,6 @@
+import torch
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 
 
@@ -17,4 +19,25 @@ class IrisDataLoader:
         return {
             "X": X,
             "y": y
+        }
+    
+
+    def transform_data(self):
+
+        data = self.data
+        X_train, X_test, y_train, y_test = train_test_split(data.get("X"),
+                                                            data.get("y"),
+                                                            test_size=0.2,
+                                                            random_state=0)
+
+        X_train = torch.tensor(X_train, dtype=torch.float32)
+        X_test = torch.tensor(X_test, dtype=torch.float32)
+        y_train = torch.tensor(y_train, dtype=torch.long)
+        y_test = torch.tensor(y_test, dtype=torch.long)
+
+        return {
+            "X_train": X_train,
+            "X_test": X_test,
+            "y_train": y_train,
+            "y_test": y_test
         }
